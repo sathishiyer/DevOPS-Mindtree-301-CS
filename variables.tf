a50a584b9f717e78a2b16c96c8516b0932fd0566
sathishiyer <64395386+sathishiyer@users.noreply.github.com>
2021-02-13 16:06:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "key_name" {
-  default = "terraform-key"
+  default = "Demo301"
 }
 
 variable "pvt_key_name" {
-  default = "/root/.ssh/terraform-pvt-key.pem"
+  default = "/root/.ssh/Demo301.pem"
 }
 
 
 variable "sg_id" {
-  default = "sg-0d3c28ab8c6633258"
+  default = "sg-01213cd8e25d42dbb"
 }
 
 variable "region" {
-  default = "us-east-2"
+  default = "ap-south-1"
 }
 
 
